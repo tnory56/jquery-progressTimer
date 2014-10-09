8dc56e69c4f701353bec3073ac86f0579a70d534
tnory56 <tnorberg@thomasnorberg.com>
2014-10-09 01:14:33 -0400

    updated to new version cleaning up some mess. 1.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,75 +2,75 @@
  * Created by thomas.c.norberg on 4/24/14.
  */
 module.exports = function (grunt) {
-    'use strict';
+    "use strict";
 
     // Force use of Unix newlines
-    grunt.util.linefeed = '\n';
+    grunt.util.linefeed = "\n";
 
     RegExp.quote = function (string) {
-        return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+        return string.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
     };
     // If you want to add the grunt css/js to another view change it here
-    var distDir = 'dist/',
-        demoDir = 'demo/',
-        srcDir = 'src/',
-        bowerDir = 'bower_components/',
-        staticJsFile = demoDir + 'js/static.js',
-        staticMinJsFile = demoDir + 'js/static.min.js',
-        staticCssFile = demoDir + 'css/static.css',
-        staticMinCssFile = demoDir + 'css/static.min.css';
+    var distDir = "dist/",
+        demoDir = "demo/",
+        srcDir = "src/",
+        bowerDir = "bower_components/",
+        staticJsFile = demoDir + "js/static.js",
+        staticMinJsFile = demoDir + "js/static.min.js",
+        staticCssFile = demoDir + "css/static.css",
+        staticMinCssFile = demoDir + "css/static.min.css";
     // 1. All configuration goes here
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        pkgName: '<%= pkg.name.replace(/-/, ".") %>',
+        pkg: grunt.file.readJSON("package.json"),
+        pkgName: "<%= pkg.name.replace(/-/, \".\") %>",
         banner: "/**!\n" +
-            " * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today('m/d/yyyy') %>\n" +
+            " * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today(\"m/d/yyyy\") %>\n" +
             " * <%= pkg.homepage %>\n" +
-            " * Copyright (c) <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>;\n" +
-            " * Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %>\n" +
+            " * Copyright (c) <%= grunt.template.today(\"yyyy\") %> <%= pkg.author.name %>;\n" +
+            " * Licensed <%= _.pluck(pkg.licenses, \"type\").join(\", \") %>\n" +
             " */\n",
         concat: {
             options: {
-                //banner: '<%= banner %>\n',
+                //banner: "<%= banner %>\n",
                 stripBanners: false
             },
             // 2. Configuration for concatenating files goes here.
             staticjs: {
                 src: [
-                    bowerDir + 'jquery/dist/jquery.js',
-                    bowerDir + 'bootstrap/dist/js/bootstrap.js'
+                    bowerDir + "jquery/dist/jquery.js",
+                    bowerDir + "bootstrap/dist/js/bootstrap.js"
                 ],
                 dest: staticJsFile
             },
             staticcss: {
                 src: [
-                    bowerDir + 'bootstrap/dist/css/bootstrap.css',
-                    bowerDir + 'bootstrap/dist/css/bootstrap-theme.css'
+                    bowerDir + "bootstrap/dist/css/bootstrap.css",
+                    bowerDir + "bootstrap/dist/css/bootstrap-theme.css"
                 ],
                 dest: staticCssFile
             },
             css: {
                 src: [
-                    [srcDir + 'css/*.css']
+                    [srcDir + "css/*.css"]
                 ],
-                dest: distDir + 'css/<%= pkgName %>.css'
+                dest: distDir + "css/<%= pkgName %>.css"
             },
             js: {
                 src: [
-                    [srcDir + 'js/*.js']
+                    [srcDir + "js/*.js"]
                 ],
-                dest: distDir + 'js/<%= pkgName %>.js'
+                dest: distDir + "js/<%= pkgName %>.js"
             }
         },
         cssmin: {
             css: {
                 files: [
                     {
-                        src: '<%= concat.css.dest %>',
-                        dest: distDir + 'css/<%= pkgName %>.min.css'
+                        src: "<%= concat.css.dest %>",
+                        dest: distDir + "css/<%= pkgName %>.min.css"
                     },
                     {
-                        src: '<%= concat.staticcss.dest %>',
+                        src: "<%= concat.staticcss.dest %>",
                         dest: staticMinCssFile
                     }
                 ]
@@ -80,11 +80,11 @@ module.exports = function (grunt) {
             js: {
                 files: [
                     {
-                        src: '<%= concat.js.dest %>',
-                        dest: distDir + 'js/<%= pkgName %>.min.js'
+                        src: "<%= concat.js.dest %>",
+                        dest: distDir + "js/<%= pkgName %>.min.js"
                     },
                     {
-                        src: '<%= concat.staticjs.dest %>',
+                        src: "<%= concat.staticjs.dest %>",
                         dest: staticMinJsFile
                     }
                 ]
@@ -96,23 +96,23 @@ module.exports = function (grunt) {
                     // includes files within path
                     {
                         expand: true,
-                        src: [ bowerDir + 'bootstrap/dist/fonts/*'],
-                        dest: demoDir + 'fonts/',
-                        filter: 'isFile',
+                        src: [ bowerDir + "bootstrap/dist/fonts/*"],
+                        dest: demoDir + "fonts/",
+                        filter: "isFile",
                         flatten: true
                     },
                     {
                         expand: true,
-                        src: [ srcDir + 'img/*'],
-                        dest: demoDir + 'img/',
-                        filter: 'isFile',
+                        src: [ srcDir + "img/*"],
+                        dest: demoDir + "img/",
+                        filter: "isFile",
                         flatten: true
                     },
                     {
                         expand: true,
-                        src: bowerDir + 'respond/dest/respond.min.js',
-                        dest: demoDir + 'js/',
-                        filter: 'isFile',
+                        src: bowerDir + "respond/dest/respond.min.js",
+                        dest: demoDir + "js/",
+                        filter: "isFile",
                         flatten: true
                     }
                 ]
@@ -121,26 +121,26 @@ module.exports = function (grunt) {
         usebanner: {
             dist: {
                 options: {
-                    position: 'top',
-                    banner: '<%= banner %>'
+                    position: "top",
+                    banner: "<%= banner %>"
                 },
                 files: {
                     src: [
-                        '<%= concat.js.dest %>',
-                        '<%= concat.css.dest %>',
-                        '<%= uglify.js.files[0].dest %>',
-                        '<%= uglify.js.files[1].dest %>',
-                        '<%= cssmin.css.files[0].dest %>',
-                        '<%= cssmin.css.files[1].dest %>',
+                        "<%= concat.js.dest %>",
+                        "<%= concat.css.dest %>",
+                        "<%= uglify.js.files[0].dest %>",
+                        "<%= uglify.js.files[1].dest %>",
+                        "<%= cssmin.css.files[0].dest %>",
+                        "<%= cssmin.css.files[1].dest %>"
                     ]
                 }
             }
         },
-        clean: [distDir + '*', demoDir + '/css/*', demoDir + '/js/*', demoDir + '/fonts/*'],
+        clean: [distDir + "*", demoDir + "/css/*", demoDir + "/js/*", demoDir + "/fonts/*"],
         jshint: {
             // You get to make the name
             // The paths tell JSHint which files to validate
-            myFiles: ['src/js/**/*.js']
+            myFiles: ["src/js/**/*.js"]
         },
         bump: {
             options: {
@@ -151,66 +151,66 @@ module.exports = function (grunt) {
                 updateConfigs: ["pkg"],
                 commitFiles: ["-a"],
                 push: true,
-                pushTo: '<%= pkg.respository.url =>'
+                pushTo: "<%= pkg.respository.url =>"
             }
         },
         jsdox: {
             generate: {
                 options: {
                     contentsEnabled: true,
-                    contentsTitle: 'Example Documentation',
-                    contentsFile: 'readme.md'//,
+                    contentsTitle: "Example Documentation",
+                    contentsFile: "readme.md"//,
                     ///pathFilter: /^example/
                 },
-                src: ['src/js/*'],
-                dest: 'md'
+                src: ["src/js/*"],
+                dest: "md"
             }//,
             // [optional additional "generation" task like generate above, can be targed with jsdox:generate-other-docs],
             /*publish: {
                 enabled: true,
-                path: '<%= jsdox.generate.dest %>',
-                message: 'Markdown Auto-Generated for version <%= pkg.version %>',
-                remoteName: 'upstream',
-                remoteBranch: 'master'
+                path: "<%= jsdox.generate.dest %>",
+                message: "Markdown Auto-Generated for version <%= pkg.version %>",
+                remoteName: "upstream",
+                remoteBranch: "master"
             }*/
         }/* Need to install grunt-sed but current version does not support exclude,
          sed: {
          versionNumber: {
          pattern: (function () {
-         var old = grunt.option('oldver');
+         var old = grunt.option("oldver");
          return old ? RegExp.quote(old) : old;
          })(),
-         replacement: grunt.option('newver'),
+         replacement: grunt.option("newver"),
          recursive: true,
-         exclude:['node_modules','bower_components']
+         exclude:["node_modules","bower_components"]
          }
          }*/
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     // measures the time each task takes
-    require('time-grunt')(grunt);
+    require("time-grunt")(grunt);
 
     // load grunt config
-    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+    require("load-grunt-tasks")(grunt, {scope: "devDependencies"});
 
-    grunt.registerTask('dev-js', ['newer:jshint:myFiles', 'newer:concat:staticjs', 'newer:concat:js', 'newer:uglify:js']);
+    grunt.registerTask("dev-js", ["newer:jshint:myFiles", "newer:concat:staticjs", "newer:concat:js", "newer:uglify:js"]);
 
-    grunt.registerTask('dist-js', ['jshint:myFiles', 'concat:staticjs', 'concat:js', 'uglify:js']);
+    grunt.registerTask("dist-js", ["jshint:myFiles", "concat:staticjs", "concat:js", "uglify:js"]);
 
-    grunt.registerTask('dev-css', ['newer:concat:staticcss', 'newer:concat:css', 'newer:cssmin:css']);
+    grunt.registerTask("dev-css", ["newer:concat:staticcss", "newer:concat:css", "newer:cssmin:css"]);
 
-    grunt.registerTask('dist-css', ['concat:staticcss', 'concat:css', 'cssmin:css']);
+    grunt.registerTask("dist-css", ["concat:staticcss", "concat:css", "cssmin:css"]);
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['dev-js', 'dev-css', 'newer:copy:main']);
+    grunt.registerTask("default", ["dev-js", "dev-css", "newer:copy:main"]);
 
-    grunt.registerTask('dist', ['clean', 'dist-js', 'dist-css', 'copy:main', 'usebanner:dist']);
+    grunt.registerTask("dist", ["clean", "dist-js", "dist-css", "copy:main", "usebanner:dist"]);
 
     // Version numbering task.
     // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
     // This can be overzealous, so its changes should always be manually reviewed!
-    //grunt.registerTask('change-version-number', 'sed');
+    //grunt.registerTask("change-version-number", "sed");
 };
 
 
